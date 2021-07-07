ce91c88543b7a01a18fc9362a3d7a4da427f718b
Daniil <cvb.amaho@gmail.com>
2021-07-07 16:43:49 +0100

    Base Table

--- GridView.js
+++ GridView.js
@@ -3,7 +3,7 @@ class GridView {
 	 *  properties
 	 * @param [array] _tableClass - css class...
 	 * @param [array] data - entry data
-	 * @param [array] _attribute (what and where from entry data)
+	 * @param [array] attribute (what and where from entry data)
 	 * @param [array] _element (where put on page)
 	 * @param [array] _header (header table)
 	 * @param [array] _headerClass css header class
@@ -14,7 +14,7 @@ class GridView {
 		this._headerClass = []
 		this._tableClass = []
 		this._element = 'body'
-		this._attribute = []
+		this.attribute = []
 	}
 	/**
 	 * Method set header
@@ -34,7 +34,7 @@ class GridView {
 
 	set headerClass(headerClass) {
 		if (typeof headerClass === 'object') {
-			this.headerClass = headerClass
+			this._headerClass = headerClass
 			return true
 		}
 		return false
@@ -46,7 +46,7 @@ class GridView {
 
 	set element(element) {
 		if (document.querySelector(element)) {
-			this.element = document.querySelector(element)
+			this.element = element
 			return true
 		}
 		return false
@@ -56,5 +56,58 @@ class GridView {
 	 * Method for show GridViewTable
 	 */
 
-	render() {}
+	render() {
+		// show header
+		if (this._header) {
+			const header = document.createElement('h1')
+			header.textContent = this._header
+			this._headerClass.forEach(cssClass => {
+				header.classList.add(cssClass)
+			})
+			document.querySelector(this._element).append(header)
+		}
+
+		// show Table
+
+		const table = document.createElement('table')
+		this._tableClass.forEach(cssClass => {
+			table.classList.add(cssClass)
+		})
+		// create table header
+		let trHeader = document.createElement('tr')
+		for (let key in this.attribute) {
+			let th = document.createElement('th')
+			if (this.attribute[key].label) {
+				th.textContent = this.attribute[key].label
+			} else {
+				th.textContent = key
+			}
+			trHeader.append(th)
+		}
+
+		table.append(trHeader)
+		// draw table
+		for (let i = 0; i < this.data.length; i++) {
+			let dataArr = this.data[i] // one string data
+			let tr = document.createElement('tr')
+			for (let key in this.attribute) {
+				let td = document.createElement('td')
+				let value = dataArr[key]
+				// is function in value?
+				if (this.attribute[key].value) {
+					value = this.attribute[key].value(dataArr)
+				}
+				// attribute src
+				if (this.attribute[key].src) {
+					td.innerHTML = value
+				} else {
+					td.textContent = value
+				}
+				tr.append(td)
+			}
+			table.append(tr)
+		}
+
+		document.querySelector(this._element).append(table)
+	}
 }
--- script.js
+++ script.js
@@ -25,3 +25,28 @@ const dataExample = [
 		country: 'USA',
 	},
 ]
+
+let gridView = new GridView()
+gridView.header = 'Hello'
+gridView.headerClass = ['header', 'Site-header']
+gridView.attribute = {
+	company: {
+		label: 'Company',
+		src: 'html',
+	},
+	chef: {
+		label: 'Director',
+	},
+	country: {
+		label: 'Country',
+		value: data => {
+			if (data['country'] === 'Czech') {
+				return data['country'] + ' map'
+			}
+			return data['country']
+		},
+	},
+}
+
+gridView.data = dataExample
+gridView.render()
